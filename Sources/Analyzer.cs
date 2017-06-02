e23682c0285b5ef3e8713a2d1e2e43082184bf35
Beau Baer <bbaer10000@gmail.com>
2017-06-02 13:10:26 -0600

    Restructured project again

---
+++ Sources/Analyzer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Managed_Assembly_Patcher
+{
+    static class Analyzer
+    {
+
+    }
+}
